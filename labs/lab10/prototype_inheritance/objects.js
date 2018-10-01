19f5e69aaec82cb7d94cb53172f4bf2ddd2132be
Zain Afzal <zain.afz@gmail.com>
2018-10-01 15:17:31 +1000

    please

---
+++ labs/lab10/prototype_inheritance/objects.js
@@ -0,0 +1,32 @@
+/*
+ * given a name and a age make a Dog object
+ * which stores this information
+ * and which has a function called
+ * toHumanYears which returns how old the
+ * Dog is in human years (1 dog year is 7 human years) (not really but lets pretend)
+ *
+ * const me = Dog("sam",91)
+ * me.name should be "sam"
+ * me.age should be 91
+ *
+ * make Dog such that it is inheriting from the provided
+ * Animal class
+ *
+ * me.__proto__ should be Animal
+ * me.__proto__.__proto__ should be Animal
+ * me.sayWoof() should return 'woof!'
+ *
+ */
+
+function Animal(age) {
+    this.age = age
+}
+
+Animal.prototype.makeSound = function() {
+    console.log(this.sound)
+}
+
+function Dog() {
+
+}
+module.exports = Dog;
