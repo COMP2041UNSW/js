276b3b0a1ef1de9da9351d46e62c7463ccc678eb
Zain Afzal <zain.afz@gmail.com>
2018-10-15 22:09:25 +1100

    removed code from collapse

--- labs/lab11/collapse/collapse.js
+++ labs/lab11/collapse/collapse.js
@@ -1,13 +1,3 @@
 (function() {
-  'use strict';
-  const NUM_CARDS = 2;
-  const cards = Array(NUM_CARDS).fill(0).map((_,i) =>`item-${i+1}`);
-  cards.map(card =>
-    document.getElementById(card).addEventListener('click', toggle)
-  );
 
-  function toggle(e) {
-    const content = document.getElementById(`${e.target.id}-content`);
-    content.style.display = 'none';
-  }
 }());
